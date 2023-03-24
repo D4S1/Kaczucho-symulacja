8f4cec2011f248b276c538298a849257300ad772
Michał Pszenicyn <pszenicynm@gmail.com>
2023-03-24 20:55:43 +0100

    Implemented generate_food

--- environment.py
+++ environment.py
@@ -1,9 +1,20 @@
+import random
+import numpy as np
+
+
 class Environment:
 
-    def __init__(self, bio_density):
+    def __init__(self, bio_density, width, height):
         self.bio_density = bio_density
-        self.env = []
+        self.width = width
+        self.height = height
+        self.env = np.zeros((height+2, width+2), dtype=int)
 
     def generate_food(self):
-        pass
-
+        tiles = self.width * self.height
+        food_number = int(self.bio_density * tiles)
+        food_tiles = random.sample(range(tiles), food_number)
+        for tile in food_tiles:
+            x = (tile // self.width) + 1
+            y = (tile % self.width) + 1
+            self.env[y][x] = 1
